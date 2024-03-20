64297abd750c84a2d21e23dd0fe6fc1b3efc06dd
jonathan <jonathan.123kutumbakana@gmail.com>
2024-03-20 15:27:01 +0300

    DONE

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', NULL, 'M', 'Brown', 'Gentle', TRUE),
+  ('Rowdy', NULL, 'M', 'Black', 'Playful', TRUE),
+  ('Tabitha', NULL, 'F', 'Gray', 'Calm', TRUE),
+  ('Sergeant Brown', NULL, 'M', 'Brown', 'Grumpy', TRUE),
+  ('Melissa', NULL, 'F', 'Honey', 'Sweet', TRUE),
+  ('Grinch', NULL, 'M', 'Green', 'Sneaky', TRUE),
+  ('Wendy', NULL, 'F', 'White', 'Friendly', TRUE),
+  (NULL, NULL, NULL, NULL, NULL, NULL); -- This creates a record with all NULL value
